16920885a622c364543eb3bead13b5047f9e4864
farhan11anh <farhan.fcbar@gmail.com>
2025-12-25 07:48:18 +0530

    updated resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -57,12 +57,15 @@ const About = () => {
         className="mt-10 px-6 py-3 text-white bg-gradient-to-r from-cyan-500 to-indigo-500 rounded-md shadow-md hover:bg-gradient-to-r hover:from-cyan-600 hover:to-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
         onClick={() =>
           window.open(
-            "https://drive.google.com/file/d/1vrYWFpb_OlY7BgEEc5-ehsHI922j3Zye/view?usp=sharing",
+            "https://drive.google.com/file/d/1JXxclNaQDOGD9BauEy1jS9XDYk5QsaMa/view?usp=sharing",
             "_blank"
           )
         }
       >
-        <span className="font-semibold flex gap-1.5 items-center"><RiBriefcase4Fill />Download Resume</span>
+        <span className="font-semibold flex gap-1.5 items-center">
+          <RiBriefcase4Fill />
+          Download Resume
+        </span>
       </button>
       <div className="mt-12 flex flex-wrap gap-10">
         {services.map((service, index) => (
